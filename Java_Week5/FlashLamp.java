d3d51fee248138fe2c7aa6fe8b5f75bc49b0e8cc
thuy-pham-2k5 <146049813+thuy-pham-2k5@users.noreply.github.com>
2024-05-24 08:46:46 +0700

    Add files via upload

---
+++ Java_Week5/FlashLamp.java
@@ -0,0 +1,15 @@
+public class FlashLamp {
+    public boolean status() {
+        return false;
+    }
+    public Battery battery() {
+        public int getBattery() {
+            return energy;
+        }
+        public void setBattery(int enery) {
+            this.enery = energy;
+        }
+    }
+
+
+}
